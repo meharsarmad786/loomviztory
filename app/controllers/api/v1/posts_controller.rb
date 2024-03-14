22314a0c408fa11bf9f2f25899223d0a9f4e5955
meharsarmad786 <mahersarmad786@gmail.com>
2024-03-15 03:43:17 +0500

    react front end setup

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -4,6 +4,7 @@ class Api::V1::PostsController < ApplicationController
   # GET /posts or /posts.json
   def index
     @posts = Post.all
+    render json: @posts
   end
 
   # GET /posts/1 or /posts/1.json
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,13 +7,14 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://127.0.0.1:3000/'  # Adjust this to allow requests from specific domains, or use '*' for all domains
+      origins 'http://127.0.0.1:5173'  # Update to correct origin of your frontend application
   
-      resource '*',  # This defines which paths are accessible, '*' all paths are accessible
-        headers: :any,  # Allows all headers
-        methods: %i[get post put patch delete options head],  # Defines allowed HTTP request methods
-        expose: ['Authorization'],  # Exposes specific headers (optional)
-        max_age: 600  # Cache the preflight requests for 10 minutes (optional)
+      resource '*',
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head],
+        expose: ['Authorization'],
+        max_age: 600
     end
   end
+  
   
